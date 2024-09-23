c13315de6feac86639134a0146495e45d2e70d36
slucaci <serjlucaci@gmail.com>
2024-09-23 17:20:30 +0100

    API Throttling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -19,11 +19,13 @@ function repoInformationHTML(repos) {
   if (repos.length == 0) {
     return `<div class="clearfix repo-list">No repos!</div>`;
   }
+
   var listItemsHTML = repos.map(function (repo) {
     return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`;
   });
+
   return `<div class="clearfix repo-list">
                 <p>
                     <strong>Repo List:</strong>
@@ -37,6 +39,7 @@ function repoInformationHTML(repos) {
 function fetchGitHubInformation(event) {
   $("#gh-user-data").html("");
   $("#gh-repo-data").html("");
+
   var username = $("#gh-username").val();
   if (!username) {
     $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -62,6 +65,13 @@ function fetchGitHubInformation(event) {
     function (errorResponse) {
       if (errorResponse.status === 404) {
         $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+      } else if (errorResponse.status === 403) {
+        var resetTime = new Date(
+          errorResponse.getResponseHeader("X-RateLimit-Reset") * 1000
+        );
+        $("#gh-user-data").html(
+          `<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`
+        );
       } else {
         console.log(errorResponse);
         $("#gh-user-data").html(
