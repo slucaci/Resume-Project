3685e3032e5a83cb58816365562ab96091c499ec
slucaci <serjlucaci@gmail.com>
2024-09-23 06:53:56 +0100

    Render user information from github

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,13 +1,34 @@
-function fetchGitHubInformation() {
-  var username = $("gh-username").val();
+function userInformationHTML(user) {
+  return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+function fetchGitHubInformation(event) {
+  var username = $("#gh-username").val();
   if (!username) {
     $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
     return;
   }
-  $("gh-user-data").html(
-    `<div id="loader"><img src="assets/css/loader.gif" alt="loading..." /> </div>`
+
+  $("#gh-user-data").html(
+    `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`
   );
-  $.when($.getJSON(`https://api.github.com/users/${slucaci}`)).then(
+
+  $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
     function (response) {
       var userData = response;
       $("#gh-user-data").html(userInformationHTML(userData));
